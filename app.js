48db523247a45abf373449badda9d8a4ffc6632d
alan78814 <tri78814@gmail.com>
2021-08-23 16:51:13 +0800

    feat: Install bcryptjs & Add userController and signup page & Install body-parser

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const handlebars = require('express-handlebars') // 引入 handlebars
 const db = require('./models') // 引入資料庫
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
 
 app.engine('handlebars', handlebars({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
