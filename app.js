b93ea62cad830bf026f2f422bc9a38ca8df443e3
alan78814 <tri78814@gmail.com>
2021-08-24 15:37:39 +0800

    feat: tInstall express-session connect-flash ＆ Confirm user password and unique email

--- app.js
+++ app.js
@@ -4,15 +4,26 @@ const db = require('./models') // 引入資料庫
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 
-app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+// 把 req.flash 放到 res.locals 裡面
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
-  
+
 // 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
 require('./routes')(app)
 
