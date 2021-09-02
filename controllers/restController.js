415c72a6adcd7f2b55deb0829d5a254093d78aa3
alan78814 <tri78814@gmail.com>
2021-09-02 21:47:21 +0800

    feat: add viewCounts increment function

--- controllers/restController.js
+++ controllers/restController.js
@@ -60,8 +60,9 @@ const restController = {
                 { model: Comment, include: [User] }
             ]
         }).then(restaurant => {
+            restaurant.increment('viewCounts')
             return res.render('restaurant', {
-                restaurant: restaurant.toJSON()
+                restaurant: restaurant.toJSON(),
             })
         })
     },
