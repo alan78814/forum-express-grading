6ba2cdfb80fa69a9c4f243f2ddd95c6269a7264c
alan78814 <tri78814@gmail.com>
2021-09-07 21:33:11 +0800

    feat: POST /api/admin/restaurants

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.engine('handlebars', handlebars({
 }))
 app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
 app.use(flash())
 app.use(passport.initialize())
